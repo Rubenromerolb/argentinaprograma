3210597c68ed8cd3ecbf53a35bffdb13bf17b22f
Rubenromerolb <xtubencitox@gmail.com>
2023-06-13 03:46:06 -0300

    Ejercicio 1

---
+++ Practico0/multiplicacion.js
@@ -0,0 +1,9 @@
+let numero1 = 2;
+let numero2 = 17;
+let numero3 = 6;
+
+let multiplacion = numero1 * numero2 * numero3;
+
+console.log("El resultado es:",multiplacion);
+
+ //Los espacios son para que se vea mas didactico, si se necesita un uso mas practico puedo quitarlos.
