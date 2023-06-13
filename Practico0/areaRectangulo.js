60675d811a6f04e840d0541fee6500fa091b87be
Rubenromerolb <xtubencitox@gmail.com>
2023-06-13 03:50:59 -0300

    Practico N0 finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,16 @@
+let altura;
+let base;
+
+//Rectangulo A
+base = 5;
+altura = 3;
+let areaRectanguloA = base * altura;
+
+//Rectangulo B
+base = 12.5;
+altura = 5.3;
+
+let areaRectanguloB = base * altura;
+
+console.log("El Area del rectangulo A es:",areaRectanguloA);
+console.log("El Area del rectangulo B es:",areaRectanguloB);
---
+++ Practico0/promedio3.js
@@ -0,0 +1,40 @@
+let promedio;
+let numero1;
+let numero2;
+let numero3;
+
+//Promerdio A
+numero1 = 5;
+numero2 = 7;
+numero3 = 9;
+promedio = (numero1+numero2+numero3)/3;
+console.log ("El primedio del ejercicio A es:",promedio);
+
+//Promedio B
+numero1 = -2;
+numero2 = -3;
+numero3 = -8;
+promedio = (numero1+numero2+numero3)/3;
+console.log ("El primedio del ejercicio A es:",promedio);
+
+//Promedio C
+numero1 = -1.2;
+numero2 = 5.3;
+numero3 = -3.3;
+promedio = (numero1+numero2+numero3)/3;
+console.log ("El primedio del ejercicio A es:",promedio);
+
+/*Tambien podria ser
+
+promedio = (5+7+9)/3;
+console.log ("El primedio del ejercicio A es:",promedio);
+
+//Promedio B
+promedio = (-2  -3 -8)/3;
+console.log ("El primedio del ejercicio A es:",promedio);
+
+//Promedio C
+promedio = (-1.2 + 5.3 -3.3)/3;
+console.log ("El primedio del ejercicio A es:",promedio);
+
+*/
