963944770d002a46225f1518bcb71816f20277d0
Rubenromerolb <xtubencitox@gmail.com>
2023-06-13 04:51:09 -0300

    Ejercicio 3

---
+++ Practico1/operacionesAritmeticas.js
@@ -0,0 +1,24 @@
+let number1 = 4;
+let number2 = 7;
+let number3 = 6;
+let number4 = 2;
+let vof;
+
+let suma = number1 + number2;
+let resta = number3 - number4;
+let resultadoFinal = suma*resta;
+
+if(resultadoFinal % 2 == 0)
+{
+    esPar = "Verdadero (es Par)"
+}
+else
+{
+    esPar = "Falso(es Impar)"
+}
+
+console.log("Mis variables iniciales fueron :", number1,",",number2,",",number3,",",number4,".La respuesta a verificar si el resultado final es par es:",esPar);
+
+//console.log("\nMis variables iniciales fueron :"+ number1+","+number2+","+number3+","+number4+".La respuesta a verificar si el resultado final es par es:"+esPar);
+
+//Nose si podia usar un if, como siempre iba a dar par creo que podria haber solo escrito el numero
