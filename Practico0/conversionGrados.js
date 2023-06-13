13894987eb51936ddbb6dabef84c844a1c4b366c
Rubenromerolb <xtubencitox@gmail.com>
2023-06-13 03:48:18 -0300

    Agrego el archivo conversionGrados.js

---
+++ Practico0/conversionGrados.js
@@ -0,0 +1,12 @@
+let fahrenheit;
+let Celsius;
+// Celsius A
+fahrenheit= 50;
+CelsiusA= (fahrenheit-32)*(5/9);
+
+//Celsius B
+fahrenheit = 75;
+CelsiusB= (fahrenheit-32)*(5/9);
+
+console.log("50 F son igual a :",CelsiusA,"C");
+console.log("75 F son igual a :",CelsiusB,"C");
