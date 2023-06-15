0f4b468f9c5d66010dd3d3a3dffb806bab550b24
Rubenromerolb <xtubencitox@gmail.com>
2023-06-15 04:39:03 -0300

    ejercicio3

---
+++ Practico2/operadoresLogicos.js
@@ -0,0 +1,18 @@
+const readlineSync= require("readline-sync")
+
+let numeroUsuario= readlineSync.question("Ingrese un numero Entero: ")
+
+if (numeroUsuario>0 && numeroUsuario%2===0)
+{
+    console.log ("El numero es positivo y par")
+}
+else if (numeroUsuario>0 && numeroUsuario%2!==0)
+{
+    console.log ("El numero es positivo y impar")
+}
+else if(numeroUsuario<0){
+    console.log ("El numero es Negativo")
+}
+else if(numeroUsuario===0){
+    console.log ("El numero es 0")
+}
